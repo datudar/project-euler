f05e6a5181df281dabadfe03b8351f49daa1e019
Daniel Raduta <datudar@gmail.com>
2017-11-02 14:46:17 -0400

    Updated Problem 3

--- 003_largest_prime_factor.py
+++ 003_largest_prime_factor.py
@@ -1,4 +1,5 @@
 # Problem 3
+#
 # The prime factors of 13195 are 5, 7, 13 and 29.
 #
 # What is the largest prime factor of a given number?
@@ -11,14 +12,10 @@ def prime_factors(n):
             prime_factors.append(d)
             n = n / d
         d = d + 1
-        if d**2 > n:
-            if n > 1:
-                prime_factors.append(n)
-            break
     return prime_factors
 
 t = int(input())
 
 for _ in range(t):
     n = int(input())
-    print(int(max(prime_factors(n))))
+    print(max(prime_factors(n)))
