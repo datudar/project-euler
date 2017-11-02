11305327767670f0359b30b7c7cb550aca66c4f4
Daniel Raduta <datudar@gmail.com>
2017-11-02 15:59:38 -0400

    Updated comments

--- 001_multiples_of_3_and_5.py
+++ 001_multiples_of_3_and_5.py
@@ -1,9 +1,24 @@
-# Problem 1
-#
+# Problem
+# =======
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #
 # Find the sum of all the multiples of 3 or 5 below N.
+#
+# Input Format
+# ============
+# First line contains T that denotes the number of test cases. This is followed
+# by T lines, each containing an integer, N.
+#
+# Constraints
+# ===========
+# 1 <= T <= 10^5
+# 1 <= N <= 10^9
+#
+# Output Format
+# =============
+# For each test case, print an integer that denotes the sum of all the 
+# multiples of 3 or 5 below N.
 
 def calc_sum(n, multiple):
     num_of_multiples = (n - 1) // multiple
--- 003_largest_prime_factor.py
+++ 003_largest_prime_factor.py
@@ -1,8 +1,25 @@
-# Problem 3
-#
+# Problem
+# =======
 # The prime factors of 13195 are 5, 7, 13 and 29.
 #
 # What is the largest prime factor of a given number?
+#
+# Input Format
+# ============
+# First line contains T, the number of test cases. This is followed by T lines
+# each containing an integer N.
+#
+# Constraints
+# ===========
+# 1 <= T <= 10
+# 10 <= N <= 10^2
+#
+# Output Format
+# =============
+# For each test case, display the largest prime factor of .
+#
+# Note: This solution receives 80 out of 100 points since it
+# times out on the last test case.
 
 def prime_factors(n):
     prime_factors = []
