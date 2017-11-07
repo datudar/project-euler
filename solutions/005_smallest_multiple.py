f72169c60a20c5e4809399886e0af65fa68a0856
Daniel Raduta <datudar@gmail.com>
2017-11-06 22:24:11 -0500

    Added problem 5

---
+++ solutions/005_smallest_multiple.py
@@ -0,0 +1,32 @@
+# Source
+# ======
+# https://www.hackerrank.com/contests/projecteuler/challenges/euler005
+#
+# Problem
+# =======
+# 2520 is the smallest number that can be divided by each of the numbers from 
+# 1 to 10 without any remainder. 
+#
+# What is the smallest positive number that is evenly divisible (divisible with
+# no remainder) by all of the numbers from 1 to N?
+#
+# Input Format
+# ============
+# First line contains T that denotes the number of test cases. This is followed 
+# by T lines, each containing an integer, N.
+#
+# Constraints
+# ============
+# 1 <= T <= 10
+# 1 <= N <= 40
+#
+# Output Format
+# =============
+# Print the required answer for each test case.
+
+from functools import reduce
+from math import gcd
+
+for _ in range(int(input())):
+    n = int(input())
+    print(reduce(lambda x, y: x * y // gcd(x, y), range(1, n+1)))
