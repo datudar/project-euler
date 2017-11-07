157725e7e31ccdd2e924bf0c52e3157b1b94159f
Daniel Raduta <datudar@gmail.com>
2017-11-06 23:07:19 -0500

    Updated problem 6

--- solutions/006_sum_square_difference.py
+++ solutions/006_sum_square_difference.py
@@ -27,9 +27,9 @@
 # =============
 # Print the required answer for each test case.
 
-t = int(input().strip())
+t = int(input())
 for _ in range(t):
-    n = int(input().strip())
-    square_of_sum = ((n)*(n+1)/2)**2
+    n = int(input())
+    square_of_sum = (n*(n+1)/2)**2
     sum_of_squares = n*(n+1)*(2*n+1)/6
-    print(int(square_of_sum-sum_of_squares))
+    print(int(square_of_sum - sum_of_squares))
