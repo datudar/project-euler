af091f1e80766e340f02aad9c79fb4810c5b55d0
Daniel Raduta <datudar@gmail.com>
2017-11-12 20:16:37 -0700

    Updated README for problem 7

--- solutions/007_10001st_prime.py
+++ solutions/007_10001st_prime.py
@@ -1,3 +1,28 @@
+# Source
+# ======
+# https://www.hackerrank.com/contests/projecteuler/challenges/euler007
+#
+# Problem
+# =======
+# By listing the first six prime numbers: 2,3,5,7,11 and 13, we can see that 
+# the 6th prime is 13. 
+#
+# What is the Nth prime number?
+#
+# Input Format
+# ============
+# First line contains T that denotes the number of test cases. This is followed 
+# by T lines, each containing an integer, N.
+#
+# Constraints
+# ============
+# 1 <= T <= 10^3
+# 1 <= N <= 10^4
+#
+# Output Format
+# =============
+# Print the required answer for each test case.
+
 import math
 
 t = int(input().strip())
